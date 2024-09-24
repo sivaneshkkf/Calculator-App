81deaccf1b6126dc63878ac27f496ab7634ab0b0
Sivaneshkkf <sivaneshkkf@gmail.com>
2024-09-25 02:00:57 +0530

    keydown

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const operands=document.getElementById("operands")
 
 const result =document.getElementById("result")
 
-const allBtn=document.querySelectorAll("#btn")
+const allBtn=document.querySelectorAll("button")
 
 // allBtn.forEach(btn => {
 //     console.log(btn)
@@ -17,22 +17,23 @@ const btnCon=document.querySelector(".btnCon")
 btnCon.addEventListener("click", (e) => {
     const txt = e.target.closest("button").textContent
     if(txt === "C"){
-        operands.innerHTML=operands.innerHTML.slice(0,-1)
+        operands.innerHTML="";
+        calculate();
     }else if(txt === "="){
-        operands.style.visibility="hidden"
+        operands.innerHTML = "";
         result.style.fontSize = "50px"
     }else if(txt === "x"){
         operands.innerHTML += "*"
+        calculate();
     }
     else{
         if(txt){
             operands.style.visibility="visible"
             result.style.fontSize = "36px"
             operands.innerHTML += txt
+            calculate();
         }
     }
-
-    calculate(operands.innerHTML);
 
 })
 
@@ -41,11 +42,37 @@ equalOperator.addEventListener("click", (e) => {
     
 })
 
+const numPattern = /^[0-9+\-*/=.()]*$/;
+document.addEventListener("keydown", (e) => {
 
-function calculate(txt){
+    e.preventDefault();
+
+    console.log(e.key);
+    
+    
+    if(numPattern.test(e.key)){
+        operands.innerHTML += e.key;
+        keyCheck(e.key)
+        calculate();
+    }
+    
+    if(e.key == "Backspace"){
+        operands.innerHTML = operands.innerHTML.slice(0,-1)
+        keyCheck(e.key)
+        calculate();
+    }
+    if(e.key == "Enter"){
+        operands.innerHTML="";
+        result.style.fontSize = "50px"
+        keyCheck(e.key)
+    }
+    
+})
+
+
+function calculate(){
     try {
-        const val = eval(txt);
-        console.log("Result:", val);
+        const val = eval(operands.innerHTML);
         if(val){
             result.innerHTML = val
         }else{
@@ -56,8 +83,49 @@ function calculate(txt){
         console.error("Error evaluating expression:", error.message);
         operands.style.color="#ef4444"
     }
-
-    
-    
+ 
 }
 
+
+
+function keyCheck(key){
+    allBtn.forEach(btn => {
+
+        if(key == "*"){
+            key="x";
+           
+        }else if(key == "Enter"){
+            key="="
+            
+        }else if(key == "Backspace"){
+            key="C"
+        }
+        
+        if(btn.innerText.trim() === key) {
+            
+            if(key == "C"){
+                setTimeOut(btn,"c","cPress")
+            }else if(key == "="){
+                setTimeOut(btn,"equals","equalsPress")
+            }else if(btn.classList.contains("numBtn")){
+                setTimeOut(btn,"numBtn","numBtnPress")
+
+            }else{
+                setTimeOut(btn,"operatorBtn","operatorBtnPress")
+            }
+         
+            
+        }
+        
+        
+    })
+}
+
+function setTimeOut(btn,normal,press){
+    btn.classList.remove(normal);
+    btn.classList.add(press);
+    setTimeout(() => {
+        btn.classList.add(normal);
+        btn.classList.remove(press);
+    }, 200);
+}
